f0a88855a934de350ae6919d6720306ba68822a0
amirrazavi <razavi77amir@gmail.com>
2025-08-21 11:59:24 +0330

    merge sort array

---
+++ src/algorithms/merge-sorted-array.rs
@@ -0,0 +1,33 @@
+fn merge(nums1: &mut Vec<i32>, m: usize, nums2: &Vec<i32>, n: usize) {
+    let mut i = m as isize - 1;
+    let mut j = n as isize - 1;
+    let mut k = (m + n) as isize - 1;
+
+    while j >= 0 {
+        if i >= 0 && nums1[i as usize] > nums2[j as usize] {
+            nums1[k as usize] = nums1[i as usize];
+            i -= 1;
+        } else {
+            nums1[k as usize] = nums2[j as usize];
+            j -= 1;
+        }
+        k -= 1;
+    }
+}
+
+fn main() {
+    let mut nums1 = vec![1, 2, 3, 0, 0, 0];
+    let nums2 = vec![2, 5, 6];
+    merge(&mut nums1, 3, &nums2, 3);
+    println!("{:?}", nums1);
+
+    let mut nums1 = vec![1];
+    let nums2 = vec![];
+    merge(&mut nums1, 1, &nums2, 0);
+    println!("{:?}", nums1);
+
+    let mut nums1 = vec![0];
+    let nums2 = vec![1];
+    merge(&mut nums1, 0, &nums2, 1);
+    println!("{:?}", nums1);
+}
